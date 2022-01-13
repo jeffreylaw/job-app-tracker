fbd6d9f7833f135b766c534e2812aa513b543621
Jeffrey Law <jylday56@gmail.com>
2022-01-13 13:44:15 -0800

    Modify create_tables for sqlite, update api.db

--- api/sql/create_tables.py
+++ api/sql/create_tables.py
@@ -3,32 +3,33 @@ import sqlite3
 conn = sqlite3.connect('api.db')
 c = conn.cursor()
 
-c.execute("DROP TABLE users")
-c.execute("DROP TABLE jobs")
+c.execute("DROP TABLE IF EXISTS users")
+c.execute("DROP TABLE IF EXISTS jobs")
 
 c.execute("""
     CREATE TABLE IF NOT EXISTS users
     (id INTEGER PRIMARY KEY,
-    username VARCHAR(250),
-    hashed_password VARCHAR(250),
-    is_active BOOL
+    user_id VARCHAR(255) UNIQUE NOT NULL,
+    username VARCHAR(255) NOT NULL,
+    hashed_password VARCHAR(255) NOT NULL,
+    is_active BOOL NOT NULL
     )
     """)
 
 c.execute("""
     CREATE TABLE IF NOT EXISTS jobs
     (id INTEGER PRIMARY KEY,
-    job_id VARCHAR(250) NOT NULL,
-    job_title VARCHAR(250) NOT NULL,
-    job_description VARCHAR(250),
-    company VARCHAR(250) NOT NULL,
+    job_id VARCHAR(255) NOT NULL,
+    job_title VARCHAR(255) NOT NULL,
+    job_description VARCHAR(255),
+    company VARCHAR(255) NOT NULL,
     salary DOUBLE,
-    link VARCHAR(250),
+    link VARCHAR(255),
     post_date DATETIME,
     applied_date DATETIME,
-    result VARCHAR(250) NOT NULL,
-    notes VARCHAR(250),
-    user_id INTEGER,
+    result VARCHAR(255) NOT NULL,
+    notes VARCHAR(255),
+    user_id VARCHAR(255),
     FOREIGN KEY (user_id) REFERENCES user(id)
     )
     """)
